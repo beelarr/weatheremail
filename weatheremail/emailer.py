205b9876b86b67482bb63c5dc2447ba87d787619
beelarr <bryonl@me.com>
2017-03-01 11:11:33 -0600

    Cleaning print statements and code

--- weatheremail/emailer.py
+++ weatheremail/emailer.py
@@ -6,14 +6,8 @@ import get_schedule
 
 def main():
     emails = get_emails.get_emails()
-    print(emails)
-
     schedule = get_schedule.get_schedule()
-    print(schedule)
-
     forecast = weather.get_weather_forecast()
-    print(forecast)
-
     sender.send_emails(emails, schedule, forecast)
 
 main()
